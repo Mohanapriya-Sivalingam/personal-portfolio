e7b3ccfe05495a3ec64bae6dae6b8c7f0a41ca63
Mohanapriya <mohanapriyasivalingam@gmail.com>
2026-05-05 17:01:32 +0530

    mail updated

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -16,8 +16,8 @@ function Contact() {
 
 function handleSubmit(e) {
   e.preventDefault();
-  console.log(contact)
-axios.post('https://personal-portfolio-backend-laae.onrender.com/api/contactms/sendmessage', contact)
+  
+axios.post('https://personal-portfolio-backend.railway.internal/api/contactms/sendmessage', contact)
 .then(response => {  console.log(response.data);
   alert("Message sent successfully!");  
   setContact({
